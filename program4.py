7e2c9969377137e24ae89be408b242af38d34c74
Gaurav Chikhale <gauravchl@users.noreply.github.com>
2017-03-18 12:59:48 +0530

    Add linear model

---
+++ program4.py
@@ -0,0 +1,13 @@
+# linear model using variables
+import tensorflow as tf
+sess = tf.Session()
+
+W = tf.Variable(.3);
+b = tf.Variable(-.3);
+x = tf.placeholder(tf.float32)
+
+linear_model = W * x + b
+
+init = tf.global_variables_initializer()
+sess.run(init) # initialize the variable
+print(sess.run(linear_model, {x: [1,2,3,4,5]}))
