4123c7fc08285c64ba50457b4f07d57675686fb3
Gaurav Chikhale <gauravchl@users.noreply.github.com>
2017-03-18 11:43:57 +0530

    program for constant nodes and operations on it

---
+++ program2.py
@@ -0,0 +1,12 @@
+# constant nodes, and operations on it
+import tensorflow as tf
+sess = tf.Session()
+
+node1 = tf.constant(4);
+node2 = tf.constant(5);
+
+node3 = node1 + node2
+node4 = tf.add(node1, node2)
+
+print(sess.run(node3))
+print(sess.run(node4))
