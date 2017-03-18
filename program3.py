aa06d3c02f2b1613f681f60a637bc8627529343e
Gaurav Chikhale <gauravchl@users.noreply.github.com>
2017-03-18 11:46:22 +0530

    program for placeholder in tensorflow

---
+++ program3.py
@@ -0,0 +1,9 @@
+# placeholder in tensorflow
+import tensorflow as tf
+sess = tf.Session()
+
+node1 = tf.placeholder(tf.int32);
+node2 = tf.placeholder(tf.int32);
+
+node3 = node1 + node2
+print(sess.run(node3, {node1: 4, node2: 6}))
