11bb2bcf214dde81d3b48f6d967d04b906c543b8
Gaurav Chikhale <gauravchl@users.noreply.github.com>
2017-08-31 14:12:29 +0530

    programm to calculate cost

---
+++ program5.py
@@ -0,0 +1,34 @@
+#!/usr/local/bin/python3
+
+# Programm to calculate cost function
+
+import os
+import tensorflow as tf
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+# Training data
+x = tf.placeholder(tf.float32) # Input
+y = tf.placeholder(tf.float32) # Output
+
+W = tf.Variable(1.)
+b = tf.Variable(2.)
+
+linear_model = W * x + b
+square_deltas = tf.square( linear_model - y )
+cost = tf.reduce_sum(square_deltas)
+
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+# print(sess.run(square_deltas, { x: [1,2, 3, 4, 5], y: [1,2, 3, 4, 5]}))
+
+print('Cost(1.0, 2.0) = %s'%sess.run(cost, { x: [1,2, 3, 4, 5], y: [1,2, 3, 4, 5]}))
+sess.run([tf.assign(W, 2), tf.assign(b, 4)])
+print('Cost(2.0, 4.0) = %s'%sess.run(cost, { x: [1,2, 3, 4, 5], y: [1,2, 3, 4, 5]}))
+
+sess.run([tf.assign(W, 1), tf.assign(b, 4)])
+print('Cost(1.0, 4.0) = %s'%sess.run(cost, { x: [1,2, 3, 4, 5], y: [1,2, 3, 4, 5]}))
+
+sess.run([tf.assign(W, 1), tf.assign(b, 0)])
+print('Cost(1.0, 0.0) = %s'%sess.run(cost, { x: [1,2, 3, 4, 5], y: [1,2, 3, 4, 5]}))
